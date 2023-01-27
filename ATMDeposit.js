d2dd85b309847b7ba539509437c447c005f7ceab
Michael Ross <102625452+Rosshoven@users.noreply.github.com>
2023-01-27 09:49:28 -0600

    first commit

---
+++ ATMDeposit.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ATMDeposit = ({ onChange, isDeposit, isValid }) => {
+    const choice = ['Deposit', 'Cash Back'];
+    console.log(`ATM isDeposit: ${isDeposit}`);
+    return (
+      <label className="label huge">
+        <h3> {choice[Number(!isDeposit)]}</h3>
+        <input id="number-input" type="number" width="200" onChange={onChange}></input>
+        <button type="submit" disabled={!isValid} width="200" value="Submit" id="submit-input">Sumbit</button>
+      </label>
+    );
+  };
+
+  export default ATMDeposit;
